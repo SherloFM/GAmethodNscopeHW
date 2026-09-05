221462a1f763ac2c99bb085df083a5faf0297b5e
SherloFM <107109880+SherloFM@users.noreply.github.com>
2026-09-05 14:52:07 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,51 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    private static int counter;
+    protected int sum;
+    public String name;
+
+    public int add(int a, int b){
+        sum = a + b;
+        counter++;
+        return sum;
+    }
+
+    protected int subtract(int a, int b){
+        sum = a-b;
+        counter++;
+        return sum;
+    }
+
+    private void resetCalculator(){
+        sum = 0;
+        System.out.println("Calculator has been reset");
+    }
+
+    public void monthlyRest(){
+        resetCalculator();
+    }
+
+    public void  showTotalCalculations(){
+        System.out.println(counter);
+    }
+
+    public void showLastResult(){
+        int sum = 100;
+        System.out.println(this.sum);
+    }
+
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.add(1,2);
+        main.subtract(50,0);
+        main.resetCalculator();
+        main.add(10000,100000);
+        main.monthlyRest();
+        main.showTotalCalculations();
+        main.subtract(200,150);
+        main.showLastResult();
     }
 }
